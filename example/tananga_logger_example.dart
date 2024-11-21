3d21e4bb7240dbcb0db6c504afa407f77aab7c20
Tananga <kamilpyrkosz97@gmail.com>
2024-11-21 20:44:53 +0100

    first commit

---
+++ example/tananga_logger_example.dart
@@ -0,0 +1,5 @@
+import 'package:tananga_logger/tananga_logger.dart';
+
+void main() {
+  TanangaLogger.log(text: "first log");
+}
---
+++ lib/src/tananga_logger_base.dart
@@ -0,0 +1,10 @@
+// TODO: Put public facing types in this file.
+
+/// Checks if you are awesome. Spoiler: you are.
+class TanangaLogger {
+  static void log({
+    required String text,
+  }) {
+    print("TANANGA: $text");
+  }
+}
